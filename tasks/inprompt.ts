604103102b4a619a26037a1e3c32addf42cc2d5e
b.truchel <b.truchel@gmail.com>
2024-03-28 19:05:42 +0000

    inprompt task

---
+++ tasks/inprompt.ts
@@ -0,0 +1,23 @@
+import { Answer, TaskResponseData } from "../types"
+import { openAIUtils } from "../utils/utils"
+
+export interface InpromptData extends TaskResponseData { input: string[], question: string }
+export interface InpromptAnswer extends Answer { answer: string }
+
+const { gpt35_completion } = openAIUtils()
+
+export async function handler(data: InpromptData): Promise<InpromptAnswer | void> {
+  const { input, question } = data
+  const nameInQuestion = getName(question)
+  if (!nameInQuestion) return
+  const context = input.filter(line => line.includes(nameInQuestion))
+  const systemPromp = `answer the question as truthly as you can. ### context: ${context.join("\n")}`
+
+  const answer = await gpt35_completion(systemPromp, question)
+  return { answer }
+}
+
+function getName(text: string): string | undefined {
+  const { groups }= text.match(/(?<name>[A-Z]\w+\b)/) || {}
+  return groups && groups.name
+}
